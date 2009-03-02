87f20dfdbd1c2693495a278a1b897a2ccc49b869
Rick Lee-Morlang <rick@lee-morlang.com>
2009-03-01 17:26:51 -0800

    fixed wait_for_text to not be sensitive to invocation order, and added support for regexes

--- lib/selenium/client/extensions.rb
+++ lib/selenium/client/extensions.rb
@@ -34,36 +34,50 @@ module Selenium
 		                       options[:timeout_in_seconds]
 		  end
 
-			# Wait for some text to be present (the wait in happenning browser side).
+			# Wait for some text to be present (the wait is happening browser side).
 			#
-			# If locator is nil or no locator is provided, the text will be
-			# detected anywhere in the page.
-			#
-			# If a non nil locator is provided, the text will be
-			# detected within the innerHTML of the element identified by the locator.			
-		  def wait_for_text(text, options={})
-        script = options[:element].nil? ?
-                 find_text_script(text, "text != null && text != false") :
-                 find_element_script(options[:element], 
-                    "element != null && element.innerHTML == '#{quote_escaped(text)}'")
-
-		    wait_for_condition script, options[:timeout_in_seconds]
-		  end
-
-			# Wait for some text to NOT be present (the wait in happenning browser side).
-			#
-			# If locator is nil or no locator is provided, the text will be
-			# detected anywhere in the page.
-			#
-			# If a non nil locator is provided, the text will be
-			# detected within the innerHTML of the element identified by the locator.			
-		  def wait_for_no_text(original_text, options={})
-        script = options[:element].nil? ?
-                 find_text_script(original_text, "(text == false || text == null)") :
-                 find_element_script(options[:element], 
-                    "(element == null || element.innerHTML != '#{quote_escaped(original_text)}')")
+      # wait_for_text will search for the given argument within the innerHTML
+      # of the current DOM. Note that this method treats a single string
+      # as a special case.
+      #
+      # ==== Parameters
+      # wait_for_text accepts an optional hash of parameters:
+      # * <tt>:element</tt> - the DOM id of an element to limit the search
+      # scope to, or a Selenium locator string.
+      # * <tt>:timeout_in_seconds</tt> - the length of time to wait for
+      # a match to appear
+      # * <tt>:invert_result</tt> - when true, the match condition is inverted.
+      # This is equivalent to calling wait_for_no_text.
+      # 
+      # ==== Regular Expressions
+      # In addition to plain strings, wait_for_text accepts regular expressions
+      # as the pattern specification. Note that the string is treated as a
+      # quoted regular expression when no <tt>:element</tt> scope is specified.
+      # 
+      # ==== Examples
+      # The following are equivalent, and will match "some text" anywhere
+      # within the document:
+      #   wait_for_text "some text"
+      #   wait_for_text /some text/
+      #
+      # This will match "some text" anywhere within the specified element:
+      #   wait_for_text /some text/, :element => "container"
+      #
+      # This will match "some text" only if it exactly matches the complete
+      # innerHTML of the specified element: 
+      #   wait_for_text "some text", :element => "container"
+      #
+      def wait_for_text(pattern, options={})
+        script = find_text_script(pattern, options)
         wait_for_condition script, options[:timeout_in_seconds]
-		  end
+      end
+      
+      # Wait for some text to NOT be present (the wait in happenning browser side).
+      #
+      # See wait_for_text for usage details.
+      def wait_for_no_text(pattern, options={})
+        wait_for_text pattern, options.merge(:invert_result => true)  
+      end
 
 			# Wait for a field to get a specific value (the wait in happenning browser side).
       def wait_for_field_value(locator, expected_value, options={})
@@ -102,17 +116,20 @@ module Selenium
         EOS
       end
 
-      def find_text_script(text, return_value)
-        script = <<-EOS
-          var text;
-
-          try {
-            text = selenium.browserbot.getCurrentWindow().find('#{quote_escaped(text)}');
-          } catch(e) {
-            text = null;
-          }
-          #{return_value};
-        EOS
+      def find_text_script(pattern, options)
+        if options[:element].nil? && !(pattern.kind_of? Regexp)
+          pattern = Regexp.new(Regexp.quote(pattern))
+        end          
+        element = options[:element] || "document.body"
+        
+        match_script = if pattern.kind_of? Regexp
+          "element != null && element.innerHTML.match(#{pattern.inspect})"
+        else
+          "element != null && element.innerHTML == '#{quote_escaped(pattern)}'"
+        end
+        match_script = "!(#{match_script})" if options[:invert_result]
+        
+        find_element_script element, match_script
       end
 
       def window_script(expression)
@@ -122,7 +139,6 @@ module Selenium
       def quote_escaped(a_string)
         a_string.gsub(/'/, %q<\\\'>)
       end
-
     end
   end
 end
