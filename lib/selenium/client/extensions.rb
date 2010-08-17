a41dbccb12d9e270db31b064d0e80f983cbcba0c
Aaron P. Tinio <aptinio@yahoo.com>
2010-08-17 17:13:08 +0800

    do not modify passed argument

--- lib/selenium/client/extensions.rb
+++ lib/selenium/client/extensions.rb
@@ -113,9 +113,9 @@ module Selenium
       end
 
       def get_css_count(css_locator)
-        css_locator.sub! /\Acss=/, ''
+        selector = JavascriptExpressionBuilder.new.quote_escaped css_locator.sub(/\Acss=/, '')
         script = <<-EOS
-          var results = eval_css('#{JavascriptExpressionBuilder.new.quote_escaped css_locator}', window.document);
+          var results = eval_css('#{selector}', window.document);
           results.length;
         EOS
         get_eval(script).to_i
